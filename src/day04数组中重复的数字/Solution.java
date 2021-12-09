688323fc1ec736e2fd40ef85c7d98d30e9a7cfbe
刘佳励 <1660208887@qq.com>
2021-12-09 22:20:10 +0800

    数组中重复的数字

--- src/day04数组中重复的数字/Solution.java
+++ src/day04数组中重复的数字/Solution.java
@@ -1,4 +1,17 @@
 package day04数组中重复的数字;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Solution {
+    public int findRepeatNumber(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for(int a : nums){
+            if(hashSet.add(a)){
+                continue;
+            }
+            return a;
+        }
+        return -1;
+    }
 }
